5adb88623d2c74812a3da68d0c098f262703bd71
adityakumar5338 <techadi005@gmail.com>
2026-04-25 14:25:15 +0530

    Add files via upload

---
+++ day_2_variable.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int variable = 10;
+
+    std::cout << variable << std::endl;
+    std::cout << variable << std::endl;
+
+    return 0;
+}
